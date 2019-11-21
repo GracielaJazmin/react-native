8d051408f8c04601ef34ac4aa6f4c1fd2002a2ad
jazmin <gracielajazmin_chavez@ucol.mx>
2019-11-21 14:43:12 -0600

    new changes

--- mimusic/src/ArtistList.js
+++ mimusic/src/ArtistList.js
@@ -9,21 +9,20 @@ import ArtistBox from './ArtistBox';
 import{Actions} from 'react-native-router-flux';
 
 export default class ArtistList extends Component <Props>{
-    
         handlePress(artist){
             Actions.artistDetail({artist:artist})
         }
-
         render(){
+          const artists = this.props.artists;
             return(
               <FlatList
-              data = {this.props.artist}
+              data = {artists}
               renderItem = {({item: artist}) => (
                    <TouchableOpacity onPress = {() => this.handlePress(artist)}>
                    <ArtistBox artist = {artist}/>
                    </TouchableOpacity>
                
-              )}></FlatList>
+              )}/>
             );
       }
     }
